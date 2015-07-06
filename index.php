f196724f39eddd09d54d41bf7035ddf77825a002
Robinson <ralvarezloud@gmail.com>
2015-07-06 12:13:47 -0500

    commit
    Creacion estilo css

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Condor</title>
+
+</head>
+<body>
+<p>Hola Gi</p>
+	
+</body>
+</html>
